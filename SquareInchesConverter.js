0bda55a60df27d1f9374d85b8d53e2a2f4ba217c
CMJ-94 <78697574+CMJ-94@users.noreply.github.com>
2021-02-24 07:43:13 +0000

    Add files via upload

---
+++ SquareInchesConverter.js
@@ -0,0 +1,9 @@
+// This is the width
+const width = 38;
+// This is the length
+const length = 200;
+// This is the square area  
+let squareArea = width*length;
+// This converts to square inches
+let squareInches = squareArea / 6.452 
+console.log(`The square inches area is ${squareInches}.`); 
